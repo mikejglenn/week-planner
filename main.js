4c19d047c07daebfa3b1e78643b1169568629aff
Ashley Avena <agshelter7@gmail.com>
2024-11-15 19:13:30 +0000

    dialog modal typescript

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-var $addEvent = document.querySelector('#add-event');
-if (!$addEvent) throw new Error('query failed');
+var $addEventButton = document.querySelector('#add-event');
+if (!$addEventButton) throw new Error('$addEventButton query failed');
+var $dialog = document.querySelector('dialog');
+if (!$dialog) throw new Error('$dialog query failed');
+var $cancelButton = document.querySelector('.cancel-button');
+if (!$cancelButton) throw new Error('$cancelButton query failed');
+var $confirmButton = document.querySelector('.confirm-button');
+if (!$confirmButton) throw new Error('$confirmButton query failed');
+$addEventButton.addEventListener('click', function () {
+  $dialog.showModal();
+});
+$cancelButton.addEventListener('click', function () {
+  $dialog.close();
+});
+$confirmButton.addEventListener('click', function () {
+  $dialog.close();
+});
--- main.ts
+++ main.ts
@@ -1,2 +1,23 @@
-const $addEvent = document.querySelector('#add-event');
-if (!$addEvent) throw new Error('query failed');
+const $addEventButton = document.querySelector('#add-event');
+if (!$addEventButton) throw new Error('$addEventButton query failed');
+
+const $dialog = document.querySelector('dialog');
+if (!$dialog) throw new Error('$dialog query failed');
+
+const $cancelButton = document.querySelector('.cancel-button');
+if (!$cancelButton) throw new Error('$cancelButton query failed');
+
+const $confirmButton = document.querySelector('.confirm-button');
+if (!$confirmButton) throw new Error('$confirmButton query failed');
+
+$addEventButton.addEventListener('click', () => {
+  $dialog.showModal();
+});
+
+$cancelButton.addEventListener('click', () => {
+  $dialog.close();
+});
+
+$confirmButton.addEventListener('click', () => {
+  $dialog.close();
+});
